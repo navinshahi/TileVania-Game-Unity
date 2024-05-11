188dc04e08b3d22afe020519dfdedd1d183ec040
Navin Shahi <navinkumarshahi0@gmail.com>
2024-05-11 23:07:55 +0530

    first commit

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickupSFX;
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player"){
+            AudioSource.PlayClipAtPoint(coinPickupSFX,Camera.main.transform.position);
+            Destroy(gameObject); 
+        }
+    }
+}
